ef79a6f6ebd46c07d5619fbb89c74cf4f2a0d917
Rodion <59920979+Radeon590@users.noreply.github.com>
2024-06-07 19:54:52 +0300

    New input system

--- Assets/Scripts/Stations/BuildingsSystem/BuildingsSystemCellSelector.cs
+++ Assets/Scripts/Stations/BuildingsSystem/BuildingsSystemCellSelector.cs
@@ -3,54 +3,82 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class BuildingsSystemCellSelector : MonoBehaviour
 {
+    [SerializeField] private PlayerInput playerInput;
     [SerializeField] private Camera camera;
     //
-    private Coroutine _currentCoroutine;
+    private IBuilding _building;
+    private UnityAction _onAssign;
+    private Vector2 currentPos;
     
     public void StartSelectionProcess(IBuilding building, UnityAction onAssign)
     {
-        if (_currentCoroutine != null)
-        {
-            throw new BuildingsSystemSelectorIsBusyException();
-        }
-        _currentCoroutine = StartCoroutine(AssignBuildingToCellCoroutine(building, onAssign));
+        _building = building;
+        _onAssign = onAssign;
+        AppendEvents();
     }
 
     public void CancelSelectionProcess()
     {
-        StopCoroutine(_currentCoroutine);
+        CleanEvents();
     }
 
-    private IEnumerator AssignBuildingToCellCoroutine(IBuilding building, UnityAction onAssign)
+    private void DoneSelectionProcess()
     {
-        while (true)
+        CleanEvents();
+    }
+
+    private void AppendEvents()
+    {
+        var pointAction = playerInput.actions["Point"];
+        pointAction.performed += OnPoint;
+        var clickEvent = playerInput.actions["Click"];
+        clickEvent.performed += OnClick;
+    }
+
+    private void CleanEvents()
+    {
+        var pointAction = playerInput.actions["Point"];
+        pointAction.performed -= OnPoint;
+        var clickEvent = playerInput.actions["Click"];
+        clickEvent.performed -= OnClick;
+    }
+
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+        currentPos = context.ReadValue<Vector2>();
+    }
+
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        try
         {
-            if (Input.GetMouseButtonDown(0))
+            Vector2 touchPosition = camera.ScreenToWorldPoint(currentPos);
+            var hit = Physics2D.Raycast(touchPosition, touchPosition);
+            if (hit)
             {
-                var mousePosition = Input.mousePosition;
-                var touchRay = camera.ScreenToWorldPoint(mousePosition);
-                var hit = Physics2D.Raycast(touchRay, Vector3.forward);
-                if (hit)
+                Debug.Log("hit");
+                if (hit.transform.gameObject.TryGetComponent(out BuildingsSystemCell cell))
                 {
-                    if (hit.transform.gameObject.TryGetComponent(out BuildingsSystemCell cell))
+                    try
                     {
-                        try
-                        {
-                            cell.Building = building;
-                            onAssign.Invoke();
-                            break;
-                        }
-                        catch (BuildingsSystemCellTakenException ex)
-                        {
-                            Debug.Log(ex.Message);
-                        }
+                        cell.Building = _building;
+                        DoneSelectionProcess();
+                        _onAssign.Invoke();
+                    }
+                    catch (BuildingsSystemCellTakenException ex)
+                    {
+                        Debug.Log(ex.Message);
                     }
                 }
             }
-            yield return new WaitForFixedUpdate();
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex);
         }
     }
 }
